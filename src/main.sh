e6e355357d1325d83ca52b8dd613838d4267014d
Udo Nonner <udo.nonner@franzk.net>
2026-05-11 19:15:41 +0200

    use set_tags function for ansible-push

--- src/main.sh
+++ src/main.sh
@@ -18,6 +18,7 @@ CYAN="\e[36m"
 # CONFIG & DEFAULTS
 PATH_CONFIG="${SCRIPT_PARENT}/config.cfg"
 PATH_DEFAULTS="${SCRIPT_DIR}/defaults.cfg"
+PATH_DATA="${SCRIPT_PARENT}/data"
 
 if [[ -r ${PATH_CONFIG} ]]; then
 	source "${PATH_CONFIG}"
@@ -25,6 +26,146 @@ else
 	echo "<4>WARN: No config file found at ${PATH_CONFIG}. Using defaults ..."
 	source "${PATH_DEFAULTS}"
 fi
+
+function set_tags {
+	# requires: 
+	# - ANSIBLE_LIST_TAGS_REFERENCE_PLAYBOOK
+	# - PATH_DATA
+	# sets:
+	# - ANSIBLE_TAGS
+	# - ANSIBLE_TAGS_ARRAY
+
+	local tags_query
+	local available_tags=()
+	local out_name=$(basename ${ANSIBLE_LIST_TAGS_REFERENCE_PLAYBOOK})
+	out_name="${out_name//.yml}" # remove .yml
+	local out_path="${PATH_DATA}/tags.${out_name}"
+	local run_tag_extraction=true
+
+	# Check if the output file already exists
+    if [[ -f "${out_path}" ]]; then
+		echo
+		echo "Tag list found: ${out_path}"
+		echo -e "${CYAN}Use this tag list?${CLEAR} (Enter)"
+		echo -e "${CYAN}Refresh tag list?${CLEAR} (r)"
+        read -p ">> " choice
+        case "${choice}" in
+            r)
+				run_tag_extraction=true;;
+            *) 
+				run_tag_extraction=false;;
+        esac
+    fi
+
+	# run_tag_extraction
+	if [[ "${run_tag_extraction}" == true ]]; then
+		# get available tags
+		echo
+		echo -e "${BLINKINK}Searching for tags associated with playbook ...${CLEAR}"
+		# Extracting TASK TAGS line
+		local output_list_tags=$(ansible-playbook --list-tags "${ANSIBLE_LIST_TAGS_REFERENCE_PLAYBOOK}")
+		# Removing the prefix and brackets
+		task_tags_line="${output_list_tags#*TASK TAGS: }" # Remove from the beginning until TASK TAGS: 
+		task_tags_line="${task_tags_line//[\[\]]/}" # Remove brackets
+		# Converting the string into an array using IFS
+		IFS=', ' read -r -a available_tags <<< "${task_tags_line}"
+
+		if [[ ${#available_tags[@]} -eq 0 ]]; then
+			echo "ERROR: Could not find any tags with playbook: ${ANSIBLE_LIST_TAGS_REFERENCE_PLAYBOOK}."
+			exit 1
+		else
+			printf "%s\n" "${available_tags[@]}" > ${out_path} && echo "Tag list saved to: ${out_path}"
+		fi
+	else
+		readarray -t available_tags < "${out_path}" && echo "Loaded ${#available_tags[@]} unique tags from file"
+	fi
+
+	# ask user
+	echo
+	echo -e "${CYAN}Enter tags${CLEAR}"
+	echo -e "${GREY}Separator: comma${CLEAR}"
+	echo -e "${GREY}Partial match is supported${CLEAR}"
+	echo -e "${GREY}Leave empty to list available hosts${CLEAR}"
+	read -p ">> " tags_query
+
+	# user inputs nothing
+	if [[ -z "${tags_query}" ]]; then
+		# show full list
+		select tag in "${available_tags[@]}"; do
+			if [ -n "${tag}" ]; then
+				ANSIBLE_TAGS="${tag}"
+				echo "-> ${tag}"
+				break
+			else
+				echo -e "${MAGENTA}Invalid choice – try again.${CLEAR}"
+				continue
+			fi
+		done
+	else
+		# query
+		tags_query=${tags_query,,} # make lowercase
+		# Convert the comma-separated string into an array
+		local tags_query_array=()
+		local matches=()
+		local no_matches=()
+		IFS=',' read -ra tags_query_array <<< "${tags_query}"
+
+		# Iterate through each tag in the query
+		# find matches
+		for query_tag in "${tags_query_array[@]}"; do
+			local this_query_matches=()
+			for tag in "${available_tags[@]}"; do
+				tag=${tag,,} # lowercase
+				if [[ ${tag} == *"${query_tag}"* ]]; then
+					this_query_matches+=("${tag}")
+				fi
+			done
+			if [[ ${#this_query_matches[@]} -eq 1 ]]; then
+				matches+=("${this_query_matches[0]}")
+				echo "-> ${this_query_matches[0]}"
+			else
+				echo -e "${CYAN}Multiple matches found for ${query_tag}. Select:${CLEAR}"
+				select choice in "${this_query_matches[@]}"; do
+					if [[ -z ${choice} ]]; then
+						echo -e "${MAGENTA}Invalid choice – try again.${CLEAR}"
+						continue
+					else
+						matches+=("${choice}")
+						echo "-> ${choice}"
+						break
+					fi
+				done
+			fi
+		done
+
+		if [[ ${#matches[@]} -eq 0 ]]; then
+			# no match
+			echo -e "${MAGENTA}Could not find any matches. Please try again.${CLEAR}"
+			# repeat
+			set_tags
+		else
+			echo -e "${CYAN}Confirm the following tags:${CLEAR} ${GREY}${matches[@]}${CLEAR} (Enter)"
+			read -p ">> " confirm
+			if [[ -z ${confirm} ]]; then
+				joined=$(printf '%s,' "${matches[@]}")
+				joined=${joined%,}   # strip the trailing comma
+				ANSIBLE_TAGS="${joined}"
+				ANSIBLE_TAGS_ARRAY=("${matches[@]}")
+			else
+				echo -e "Restart."
+				# repeat
+				set_tags
+			fi
+		fi
+	fi
+
+	if [[ -n ${ANSIBLE_TAGS} ]]; then
+		return 0
+	else
+		echo "ERROR: ANSIBLE_TAGS not set!"
+		return 1
+	fi
+}
 
 function main {
 	
@@ -41,43 +182,14 @@ function main {
 
 	# Increment to get the new updateID
 	local new_updateID=$((max_updateID + 1))
-
-	# Prompt the user for tags
-	echo -e "${CYAN}Enter tags to update${CLEAR}"
-	echo -e "${GREY}Separator: comma${CLEAR}"
-	echo -e "${GREY}Leave empty for suggestions${CLEAR}"
-	read -p ">> " -r tags
 	
-	local tags_array=()
-
-	if [[ -z "${tags}" ]]; then
-		
-		if [[ -z ${ANSIBLE_LIST_TAGS_REFERENCE_PLAYBOOK} ]]; then
-			echo "ERROR: Required variable empty: ANSIBLE_LIST_TAGS_REFERENCE_PLAYBOOK"
-			exit 1
-		fi
-
-		# Extract the TASK TAGS line
-		local output_list_tags=$(ansible-playbook --list-tags "${ANSIBLE_LIST_TAGS_REFERENCE_PLAYBOOK}")
-		# Removing the prefix and brackets
-		task_tags_line="${output_list_tags#*TASK TAGS: }" # Remove from the beginning until TASK TAGS: 
-		task_tags_line="${task_tags_line//[\[\]]/}" # Remove brackets
-		# Converting the string into an array using IFS
-		IFS=', ' read -r -a tags_array <<< "${task_tags_line}"
-
-		select tag in "${tags_array[@]}"; do
-			if [ -n "${tag}" ]; then
-				echo "-> ${tag}"
-				tags="${tag}"
-				break
-			else
-				echo "Invalid selection. Try again."
-			fi
-		done			
-	fi
+	# Tags
+	set_tags
+	echo "ANSIBLE_TAGS: ${ANSIBLE_TAGS}"
+	echo "ANSIBLE_TAGS_ARRAY: ${ANSIBLE_TAGS_ARRAY[@]}"
 
 	# Convert the input into a JSON array format
-	tags_array=$(echo "${tags}" | tr ',' '\n' | jq -R . | jq -s .)
+	tags_array=$(echo "${ANSIBLE_TAGS}" | tr ',' '\n' | jq -R . | jq -s .)
 
 	if [[ ${#tags_array[@]} -eq 0 ]]; then
 		echo "No Tags given. Exit."
